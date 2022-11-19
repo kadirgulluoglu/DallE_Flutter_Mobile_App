7378b639b429a4f6f4fdb762c9f7da6ebd445f9f
kadirgulluoglu <62450370+kadirgulluoglu@users.noreply.github.com>
2022-11-19 12:10:25 +0300

    Update history_search_view.dart

--- lib/features/history_search/view/history_search_view.dart
+++ lib/features/history_search/view/history_search_view.dart
@@ -1,19 +1,19 @@
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:ui';
+
 import 'package:dalle_flutter_mobile_app/core/extension/context_extensions.dart';
+import 'package:dalle_flutter_mobile_app/features/home/model/dalle_model.dart';
 import 'package:dalle_flutter_mobile_app/features/home/viewmodel/home_viewmodel.dart';
 import 'package:dalle_flutter_mobile_app/product/components/background_image.dart';
+import 'package:dalle_flutter_mobile_app/product/components/image_container.dart';
 import 'package:dalle_flutter_mobile_app/product/components/large_text.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../../product/components/glass_box.dart';
 import '../../../product/components/text.dart';
-import '../../../product/enum/cache_status_enum.dart';
-import '../../../product/enum/view_state.dart';
 
 class HistorySearchView extends StatelessWidget {
   const HistorySearchView({Key? key}) : super(key: key);
-
+  final String _historyText = "🕰 Geçmiş Aramalar";
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<HomeViewModel>(context);
@@ -21,93 +21,109 @@ class HistorySearchView extends StatelessWidget {
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         centerTitle: true,
-        title: CustomLargeText(text: "🕰 Geçmiş Aramalar"),
+        title: CustomLargeText(text: _historyText),
         backgroundColor: Colors.transparent,
       ),
       body: Stack(
         children: [
           BackgroundImage(context: context),
-          viewModel.cacheStatus == CacheStatus.available
-              ? Column(
-                  children: [
-                    const SizedBox(height: 30),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: viewModel.modelList?.length ?? 0,
-                        itemBuilder: (context, index) {
-                          var historyModel = viewModel.modelList?[index];
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: context.paddingLeft,
-                                child: CustomText(
-                                  text: historyModel?.title
-                                          .toString()
-                                          .firstUpperCase() ??
-                                      "",
-                                  size: 20,
-                                ),
-                              ),
-                              SizedBox(
-                                height: context.height * 0.4,
-                                child: ListView.builder(
-                                  itemCount: historyModel?.data?.length ?? 0,
-                                  scrollDirection: Axis.horizontal,
-                                  itemBuilder: (context, index) {
-                                    return Container(
-                                      padding: context.paddingLow,
-                                      margin: context.paddingLow,
-                                      child: ClipRRect(
-                                        borderRadius: context.containerRadius,
-                                        child: SizedBox(
-                                          height: context.height * .4,
-                                          child: CachedNetworkImage(
-                                            imageUrl: historyModel
-                                                    ?.data?[index].url ??
-                                                "",
-                                            progressIndicatorBuilder: (context,
-                                                    url, downloadProgress) =>
-                                                GlassContainer(
-                                              width: context.height * .3,
-                                              child: Center(
-                                                child:
-                                                    CircularProgressIndicator(
-                                                        value: downloadProgress
-                                                            .progress),
-                                              ),
-                                            ),
-                                            fit: BoxFit.fill,
-                                            errorWidget:
-                                                (context, url, error) =>
-                                                    Container(),
-                                          ),
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                )
-              : Expanded(child: _buildFirstTimeText(viewModel)),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaY: 50, sigmaX: 50),
+            child: Container(),
+          ),
+          Column(
+            children: [
+              _buildSpacer(context),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: viewModel.modelList?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    var historyModel = viewModel.modelList?[index];
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildTitle(context, historyModel),
+                        _buildImageList(context, historyModel),
+                        _buildDivider(context),
+                        _buildSpacer(context),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
 
-  Center _buildFirstTimeText(HomeViewModel viewModel) {
+  SizedBox _buildSpacer(BuildContext context) {
+    return SizedBox(
+      height: context.height * .03,
+    );
+  }
+
+  Center _buildDivider(BuildContext context) {
     return Center(
+      child: Container(
+        width: context.width * .95,
+        height: 3,
+        color: Colors.grey.withOpacity(.2),
+      ),
+    );
+  }
+
+  SizedBox _buildImageList(BuildContext context, DallEModel? historyModel) {
+    return SizedBox(
+      height: context.height * 0.4,
+      child: ListView.builder(
+        itemCount: historyModel?.data?.length ?? 0,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return ImageContainer(
+            context: context,
+            model: historyModel,
+            index: index,
+          );
+        },
+      ),
+    );
+  }
+
+  // Container _buildImageContainer(
+  //     BuildContext context, DallEModel? historyModel, int index) {
+  //   return Container(
+  //     padding: context.paddingLow,
+  //     margin: context.paddingLow,
+  //     child: ClipRRect(
+  //       borderRadius: context.containerRadius,
+  //       child: SizedBox(
+  //         height: context.height * .4,
+  //         child: CachedNetworkImage(
+  //           imageUrl: historyModel?.data?[index].url ?? "",
+  //           progressIndicatorBuilder: (context, url, downloadProgress) =>
+  //               GlassContainer(
+  //             width: context.height * .3,
+  //             child: Center(
+  //               child:
+  //                   CircularProgressIndicator(value: downloadProgress.progress),
+  //             ),
+  //           ),
+  //           fit: BoxFit.fill,
+  //           errorWidget: (context, url, error) => Container(),
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  Padding _buildTitle(BuildContext context, DallEModel? historyModel) {
+    return Padding(
+      padding: context.paddingLeft,
       child: CustomText(
-        text: viewModel.state == ViewState.error
-            ? "Hadi şimdi DALL-E ile yazıyı resme çevirin"
-            : "",
-        isCenter: true,
+        text: historyModel?.title.toString().firstUpperCase() ?? "",
+        size: 20,
       ),
     );
   }
